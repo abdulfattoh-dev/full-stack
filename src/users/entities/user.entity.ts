467eae27c533ce0c80381142ee312a0a9db2a9d6
Abdulfattoh <a6du.xakim0v@gmail.com>
2025-05-23 10:19:12 +0500

    all files added

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,17 @@
+import { Column, DataType, Model, Table } from "sequelize-typescript";
+
+
+@Table({ tableName: "users" })
+export class User extends Model {
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    email: string
+
+    @Column({
+        type: DataType.STRING,
+        allowNull: false
+    })
+    password: string
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from './entities/user.entity';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel(User) private model: typeof User
+  ) { }
+
+  async create(createUserDto: CreateUserDto) {
+    const newUser = await this.model.create({ ...createUserDto });
+    return newUser;
+  }
+
+  async findAll() {
+    const users = await this.model.findAll();
+    return users;
+  }
+
+  async findOne(id: number) {
+    const user = await this.model.findByPk(id);
+    return user;
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const user = await this.model.update(updateUserDto, { where: { id }, returning: true });
+    return user[1][0];
+  }
+
+  async remove(id: number) {
+    await this.model.destroy({ where: { id } });
+    return {};
+  }
+}
